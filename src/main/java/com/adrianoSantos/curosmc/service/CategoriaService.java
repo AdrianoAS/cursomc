7737b37db4b93f1e6e7ce20e4fa9a6edaaa0345f
Adriano Araujo <aadriano990@gmail.com>
2018-09-10 18:23:01 -0300

    Listando categoria com DTO

--- src/main/java/com/adrianoSantos/curosmc/service/CategoriaService.java
+++ src/main/java/com/adrianoSantos/curosmc/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.adrianoSantos.curosmc.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,5 +39,9 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possuir produtos");
 		}
 		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
