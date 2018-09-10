3fc2cfe05556558a748110638616a4e1aecd077f
Adriano Araujo <aadriano990@gmail.com>
2018-09-10 18:58:57 -0300

    Paginas com parametros opionais na requisição

--- src/main/java/com/adrianoSantos/curosmc/service/CategoriaService.java
+++ src/main/java/com/adrianoSantos/curosmc/service/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.adrianoSantos.curosmc.domain.Categoria;
@@ -43,5 +46,10 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page,Integer linesPages,String orderBy,String direction){
+		PageRequest pageResquest = PageRequest.of(page, linesPages,Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageResquest);
 	}
 }
