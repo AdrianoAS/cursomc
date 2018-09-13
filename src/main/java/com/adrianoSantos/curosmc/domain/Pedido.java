1500fa6f83f0cf83a2e21f3da6a862a053635dfd
Adriano Araujo <aadriano990@gmail.com>
2018-09-13 20:16:18 -0300

    SubTotal e total do pedido

--- src/main/java/com/adrianoSantos/curosmc/domain/Pedido.java
+++ src/main/java/com/adrianoSantos/curosmc/domain/Pedido.java
@@ -53,6 +53,13 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	public Double getValorTotol() {
+		Double soma = 0.0;
+		for(ItemPedido ip : itens) {
+			soma = soma + ip.getSubtotal();
+		}
+		return soma;
+	}
 
 	public Integer getId() {
 		return id;
--- src/main/java/com/adrianoSantos/curosmc/repositorys/ProdutoRepository.java
+++ src/main/java/com/adrianoSantos/curosmc/repositorys/ProdutoRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.adrianoSantos.curosmc.domain.Categoria;
 import com.adrianoSantos.curosmc.domain.Produto;
@@ -17,6 +18,9 @@ import com.adrianoSantos.curosmc.domain.Produto;
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 	
+	@Transactional(readOnly=true)
 	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
 	Page<Produto> search(@Param("nome") String nome, @Param("categorias")List<Categoria> categorias, Pageable pageResquest);
+	//ou
+	//Page<Produto> findDistinctByNomeContainingandCategoriasIn(String nome, categorias, Pageable pageResquest);
 }
