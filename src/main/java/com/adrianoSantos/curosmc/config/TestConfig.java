d53aaa2dcfcb1899e219e60993ccea379b602dc2
Adriano Araujo <aadriano990@gmail.com>
2018-09-14 19:12:04 -0300

    Criando profile de teste

---
+++ src/main/java/com/adrianoSantos/curosmc/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.adrianoSantos.curosmc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.adrianoSantos.curosmc.service.BDService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private BDService dbservice;
+	
+	@Bean
+	public boolean inistantiateDataBase() throws ParseException {
+		dbservice.instantiateTesteDataBase();
+		return true;
+	}
+}
