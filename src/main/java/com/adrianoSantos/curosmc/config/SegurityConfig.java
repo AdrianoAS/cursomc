65d5c6c9c9cf16491e10fd091d7b09a8af61953c
Adriano Araujo <aadriano990@gmail.com>
2018-09-19 17:19:15 -0300

    Implementando Autorizacao

--- src/main/java/com/adrianoSantos/curosmc/config/SegurityConfig.java
+++ src/main/java/com/adrianoSantos/curosmc/config/SegurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.adrianoSantos.curosmc.security.JWTAuthorizationFilter;
 import com.adrianoSantos.curosmc.security.JwtAuthenticationFilter;
 import com.adrianoSantos.curosmc.security.JwtUtil;
 
@@ -55,6 +56,7 @@ public class SegurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JwtAuthenticationFilter(authenticationManager(),jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
