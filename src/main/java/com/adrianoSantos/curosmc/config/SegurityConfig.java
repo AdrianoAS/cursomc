6b52e94074ba6f45595276e239d48155b6a8ab33
Adriano Araujo <aadriano990@gmail.com>
2018-09-20 16:18:51 -0300

    Autorizando endpoits para usuarios especificos

--- src/main/java/com/adrianoSantos/curosmc/config/SegurityConfig.java
+++ src/main/java/com/adrianoSantos/curosmc/config/SegurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -25,6 +26,7 @@ import com.adrianoSantos.curosmc.security.JwtUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled= true)
 public class SegurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
@@ -39,8 +41,11 @@ public class SegurityConfig extends WebSecurityConfigurerAdapter {
 	};
 	public static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
-			"/categorias/**",
-			"/clientes/**"
+			"/categorias/**"
+	};
+	public static final String[] PUBLIC_MATCHERS_POST = {
+			"/clientes/**",
+			
 	};
 	
 	@Override
@@ -52,6 +57,7 @@ public class SegurityConfig extends WebSecurityConfigurerAdapter {
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
+			.antMatchers(HttpMethod.POST,PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
