89b991fc6e7bfadf6ce4281e49c1e92cf7f7c52b
Adriano Araujo <aadriano990@gmail.com>
2018-09-26 16:20:22 -0300

    Usando padrao de nomes para imagem

--- src/main/java/com/adrianoSantos/curosmc/service/ClienteService.java
+++ src/main/java/com/adrianoSantos/curosmc/service/ClienteService.java
@@ -1,10 +1,12 @@
 package com.adrianoSantos.curosmc.service;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -34,8 +36,6 @@ public class ClienteService {
 	@Autowired 
 	ClienteRepository repo;
 	
-	@Autowired
-	private ClienteService clienteService;
 	
 	@Autowired
 	private BCryptPasswordEncoder pe;
@@ -45,6 +45,13 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 	
 	
 	public Cliente find(Integer id) {
@@ -116,10 +123,11 @@ public class ClienteService {
 		if(user == null) {
 			throw new AuthorizationException("Acesso negado");
 		}
-		URI uri =  s3Service.uploadFile(multipartFile);
-		Cliente cli = clienteService.find(user.getId());
-		cli.setImageUrl(uri.toString());
-		repo.save(cli);
-		return uri;
+		
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
+		
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
+		
 	}
 }
