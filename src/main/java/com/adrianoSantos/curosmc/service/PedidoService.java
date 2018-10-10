944216c9e475e71457aeed0bbd52eae31e8e34f9
Adriano Araujo <aadriano990@gmail.com>
2018-10-10 19:02:44 -0300

    Resajustes do back end

--- src/main/java/com/adrianoSantos/curosmc/service/PedidoService.java
+++ src/main/java/com/adrianoSantos/curosmc/service/PedidoService.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
@@ -47,6 +48,7 @@ public class PedidoService {
 	}
 	
 	@Transactional
+	@Bean
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
